46b55efa012fa45d1e39be4b2b819d3012464591
Marek Grabowski <marek@marekgrabowski.net>
2023-10-20 00:23:43 -0400

    Rebuilt Nav Menu Rebuilt Nav menu from static -> react Currently includes a mobile menu & desktop menu's, planning on adding current page detection & drop downs.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import NavMenu from "./Navbar.jsx";
+import { useState } from "react";
+import Sidebar from "./Sidebar.jsx";
+
+
+const Header = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className=" bg-blue-500 shadow-md">
+      <div className="flex p-2 gap-8 justify-between sm:justify-center px-8">
+        <div className="flex items-center">
+          <img className="w-14" src="src/assets/The_Hartford_Financial_Services_Group_logo.svg" alt="Hartford Logo" />
+        </div>
+        <div className="flex items-center z-20">
+          <NavMenu />
+        </div>
+        <div className="flex sm:hidden items-center">
+          <button
+            onClick={() => setOpen(true)}
+            aria-expanded={open}
+            aria-controls="sidebar"
+            aria-label="Open Menu"
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#FFFFFF" viewBox="0 0 256 256"><path d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z"></path></svg>
+          </button>
+          <Sidebar open={open} setOpen={setOpen} />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		fontFamily: {
+		  'sans': ['Roboto', 'ui-sans-serif', 'system-ui'],
+		  'serif': ['Nunito','ui-serif', 'Georgia'],
+		  'mono': ['Roboto Mono','ui-monospace', 'SFMono-Regular'],
+		  'display': ['Oswald'],
+		  'body': ['"Open Sans"'],
+		},
+	  },
 	plugins: [],
 }
