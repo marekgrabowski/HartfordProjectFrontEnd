72c3b3ed5c8fddbeddd2097be565915ae36dcdcc
Marek Grabowski <marek@marekgrabowski.net>
2023-10-22 21:41:32 -0400

    working on updated search utlizing api old search does all filtering on front end, updated serach will send api queries for each input

--- src/components/VehicleForm-old.jsx
+++ src/components/VehicleForm-old.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Select from 'react-select';
 
 const VehicleForm = () => {
+  const [vehicleList, setVehicleList] = useState([]);
   const [makes, setMakes] = useState([]);
   const [models, setModels] = useState([]);
   const [years, setYears] = useState([]);
@@ -10,90 +11,65 @@ const VehicleForm = () => {
   const [selectedYear, setSelectedYear] = useState(null);
 
   useEffect(() => {
-    const fetchMakes = async () => {
+    const fetchData = async () => {
       try {
         const headers = {
           'Authorization': 'allow',
           'Content-Type': 'application/json',
         };
-
+  
         const requestOptions = {
           method: 'GET',
           headers: headers,
         };
-
-        const response = await fetch("https://13g2g9a95h.execute-api.us-east-1.amazonaws.com/test1/vehicles/GetAllMakes", requestOptions);
+  
+        const response = await fetch("https://13g2g9a95h.execute-api.us-east-1.amazonaws.com/test1/vehicles", requestOptions);
         const data = await response.json();
-        setMakes(data.body);
+        setVehicleList(data.body);
+
+        // Extract unique makes, models, and years
+        const uniqueMakes = [...new Set(data.body.map(vehicle => vehicle.make))];
+        const makeModels = {};
+        uniqueMakes.forEach(make => {
+          const uniqueModels = [...new Set(data.body.filter(vehicle => vehicle.make === make).map(vehicle => vehicle.model))];
+          makeModels[make] = uniqueModels;
+        });
+
+        const modelYears = {};
+        data.body.forEach(vehicle => {
+          if (!modelYears[vehicle.model]) {
+            modelYears[vehicle.model] = [];
+          }
+          modelYears[vehicle.model].push(vehicle.year);
+        });
+
+        setMakes(uniqueMakes);
+        setModels(makeModels);
+        setYears(modelYears);
       } catch (error) {
-        console.error('Error fetching makes:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchMakes();
-  }, [selectedMake]);
-  
-  const fetchModels = async () => {
-    if (selectedMake) {
-      try {
-        const headers = {
-          'Authorization': 'allow',
-          'Content-Type': 'application/json',
-        };
-
-        const requestOptions = {
-          method: 'GET',
-          headers: headers,
-        };
-
-        const response = await fetch(`https://13g2g9a95h.execute-api.us-east-1.amazonaws.com/test1/vehicles/GetModelsForMake?make=${selectedMake.value}`, requestOptions);
-        const data = await response.json();
-        console.log(data)
-        setModels(data.body);
-      } catch (error) {
-        console.error('Error fetching models:', error);
-      }
-    }
-  };
-
-  const fetchYears = async () => {
-    if (selectedModel) {
-      try {
-        const headers = {
-          'Authorization': 'allow',
-          'Content-Type': 'application/json',
-        };
-
-        const requestOptions = {
-          method: 'GET',
-          headers: headers,
-        };
-
-        const response = await fetch(`YOUR_YEARS_API_ENDPOINT?make=${selectedMake.value}&model=${selectedModel.value}`, requestOptions);
-        const data = await response.json();
-        setYears(data.body);
-      } catch (error) {
-        console.error('Error fetching years:', error);
-      }
-    }
-  };
+    fetchData();
+  }, []);
 
   const makeOptions = makes.map(make => ({
-    value: make.make_id,
-    label: make.make_name,
+    value: make,
+    label: make,
   }));
 
   const modelOptions = selectedMake
-    ? models.map(model => ({
-        value: model.model_id,
-        label: model.model_name,
+    ? models[selectedMake.value].map(model => ({
+        value: model,
+        label: model,
       }))
     : [];
 
   const yearOptions = selectedModel
-    ? years.map(year => ({
-        value: year.year_name,
-        label: year.year_name,
+    ? years[selectedModel.value].map(year => ({
+        value: year,
+        label: year,
       }))
     : [];
 
@@ -101,13 +77,11 @@ const VehicleForm = () => {
     setSelectedMake(selectedOption);
     setSelectedModel(null);
     setSelectedYear(null);
-    fetchModels();
   };
 
   const handleModelChange = (selectedOption) => {
     setSelectedModel(selectedOption);
     setSelectedYear(null);
-    fetchYears();
   };
 
   const handleYearChange = (selectedOption) => {
@@ -128,7 +102,7 @@ const VehicleForm = () => {
   const isButtonVisible = selectedMake && selectedModel && selectedYear;
 
   return (
-    <div className="flex flex-col gap-2 p-8 items-center grow">
+    <div class="flex flex-col gap-2 p-8 items-center grow ">
       <Select
         options={makeOptions}
         value={selectedMake}
@@ -154,11 +128,11 @@ const VehicleForm = () => {
       />
       {isButtonVisible && (
         <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded w-1/2 lg:w-1/4"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
+    className={`bg-blue-500 text-white font-bold py-2 px-4 rounded w-1/2 lg:w-1/4 opacity-0}`}
+    onClick={handleSubmit}
+  >
+    Submit
+  </button>
       )}
     </div>
   );
